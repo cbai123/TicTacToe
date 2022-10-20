adf899b0b43fd7386763dc7ef1e6e01b9e328475
Chris <c.bailey00@hotmail.com>
2022-10-20 12:25:08 +0100

    Created game requirements, and code to print board

---
+++ game/lib/board_print.rb
@@ -0,0 +1,7 @@
+def board_print(board, dash)
+    puts "#{board[0]}|#{board[1]}|#{board[2]}"
+    puts dash
+    puts "#{board[3]}|#{board[4]}|#{board[5]}"
+    puts dash
+    puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
---
+++ game/ttt.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+$LOAD_PATH << "./lib"
+require("./board_print.rb")
+
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+dash = "--------"
+win = false
+stalemate = false
+
+
+while true do
+    
+    board_print(board, dash)
+    break
+
+end
+
